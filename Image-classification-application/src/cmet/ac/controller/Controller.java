6af4340292a426992fde10b5b291e1aeb9d9b285
Luke Barratt <st20121473@outlook.cardiffmet.ac.uk>
2021-04-01 15:07:19 +0100

    Functionality to converted training images and test image to gray scale

--- Image-classification-application/src/cmet/ac/controller/Controller.java
+++ Image-classification-application/src/cmet/ac/controller/Controller.java
@@ -25,6 +25,7 @@ public class Controller {
 	Model model;
 	MainView mainview;
 	
+	
 	/**
 	 * @param model
 	 * @param mainview
@@ -56,6 +57,13 @@ public class Controller {
 			this.mainview.showMessage("Error in Reading the CIFAR-10 data", "File Read Error", JOptionPane.ERROR_MESSAGE);
 		}
 		this.model.setCifarimage_list((List<CIFARImage>) this.model.getCifarreader().getData());
+		
+		//convert to training data image to gray scale
+		for(int i = 0; i < this.model.getCifarimage_list().size(); i++) {
+			BufferedImage	grayScaleImage;
+			grayScaleImage = this.model.getCifarimage_list().get(i).getBuf_image();
+			this.model.convertToGrayscale(grayScaleImage);
+		}
 
 	}
 	
@@ -73,6 +81,7 @@ public class Controller {
 	
 	
 	private void readTestImageAction() {
+		//read in test image
 		try {
 			this.model.getTestImageReader().readTestImage();
 		} catch (IOException e) {
@@ -80,10 +89,16 @@ public class Controller {
 		}
 		this.model.setTestImage((BufferedImage) this.model.getTestImageReader().getTestImage());
 		
+		//Output test image to display panel
 		BufferedImage buffImage = null;
 		buffImage = this.model.getTestImage();
-		this.mainview.getDisplaypanel().getImageLbl().setIcon(new ImageIcon(buffImage));
-	
+//		this.mainview.getDisplaypanel().getImageLbl().setIcon(new ImageIcon(buffImage));
+		
+		// Converts test image to gray scale and outputs to display panel -  testing functionality
+		BufferedImage greyScaleImage = null; 
+		greyScaleImage = this.model.convertToGrayscale(buffImage);
+		this.mainview.getDisplaypanel().getImageLbl().setIcon(new ImageIcon(greyScaleImage));
+		 
 	}
 	
 	private void imageOpenAction() {
@@ -95,9 +110,7 @@ public class Controller {
 		
 		//update view
 		this.mainview.getTestimagepanel().getImageNameTxt().setText(image_path);
-		
-		
-
 	}
+	
 	
 }
--- Image-classification-application/src/cmet/ac/model/Model.java
+++ Image-classification-application/src/cmet/ac/model/Model.java
@@ -16,17 +16,51 @@ import cmet.ac.fileio.TestImageReader;
  */
 public class Model {
 	
-	List<CIFARImage> cifarimage_list;
-	IFileReader imagereader;
+	List<CIFARImage> 	cifarimage_list;
+	IFileReader 		imagereader;
 	
 	TestImageReader		testImageReader;
-	BufferedImage		testImage;
 	
-	//Need to update class to add test image reader?
+	BufferedImage		testImage;
+	BufferedImage 		grayScaleImg;
 	
 	public Model() {
 		imagereader = new CIFARReader();
 		testImageReader = new TestImageReader();
+	}
+	
+	public BufferedImage convertToGrayscale(BufferedImage img) {
+		
+		this.grayScaleImg = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+		
+		int width = img.getWidth();
+		int height = img.getHeight();
+		
+		int[] source = img.getRGB(0, 0, width, height, null, 0, width);
+		int[] destination = new int[source.length];
+		
+		for(int y = 0; y < img.getHeight(); y++) {
+			for(int x = 0; x < img.getWidth(); x++) {
+				
+				int rgbvalue = source[(y) * width + (x)];
+				
+				int alpha = (rgbvalue >> 24) & 0xff;
+				int red = (rgbvalue >> 16) & 0xff;
+				int green = (rgbvalue >> 8) & 0xff;
+				int blue = (rgbvalue) & 0xff;
+				
+				int grayscale = (int) ((0.3 * red) + (0.59 * green) + (0.11 * blue));
+				
+				int new_pixel_value = 0xFF000000 | ( grayscale << 16 ) |
+						(grayscale << 8 ) |
+						(grayscale);
+				
+				destination[(y)* width + (x)] = new_pixel_value;
+			}
+		}
+		this.grayScaleImg.setRGB(0, 0, width, height, destination, 0, width);
+		
+		return this.grayScaleImg;
 	}
 	
 
